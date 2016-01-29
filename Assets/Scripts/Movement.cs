d86c36696ddaf01b9377e48a8373e1db28035ad7
Vincent <vincent.bettenfeld@gmail.com>
2016-01-29 22:57:17 +0100

    ajout map et sprint

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,31 +10,44 @@ public class Movement : MonoBehaviour {
 	public KeyCode codeSprint = KeyCode.LeftShift;
 	public KeyCode codePositif = KeyCode.E;
 	public KeyCode codeNegatif = KeyCode.R;
-	public float speed = 2;
-	public Vector3 mouvement;
+	public float initialSpeed = 2;
+	private float currentSpeed;
+	private Vector3 mouvement;
+	Component spriteOriginal;
+	public Sprite Avecunobjet;
 
 
 	// Use this for initialization
 	void Start () {
-		
+		spriteOriginal = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		mouvement = new Vector3 (0, 0, 0);
-
+		currentSpeed = initialSpeed;
+		if (Input.GetKey (codeSprint)) {
+			currentSpeed=2*currentSpeed;
+		}
 		if (Input.GetKey (codeHaut)) {
-			mouvement += new Vector3 (0, speed, 0);
+			mouvement += new Vector3 (0, currentSpeed, 0);
 		}
 		if (Input.GetKey (codeBas)) {
-			mouvement += new Vector3 (0, -speed, 0);
+			mouvement += new Vector3 (0, -currentSpeed, 0);
 		}
 		if (Input.GetKey (codeDroite)) {
-			mouvement += new Vector3 (speed, 0, 0);
+			mouvement += new Vector3 (currentSpeed, 0, 0);
 		}
 		if (Input.GetKey (codeGauche)) {
-			mouvement += new Vector3 (-speed, 0, 0);
+			mouvement += new Vector3 (-currentSpeed, 0, 0);
 		}
+
+		if (Input.GetKeyDown (codePositif)) {
+			Debug.Log("Interaction positive");
+			GetComponent<SpriteRenderer>().sprite = Avecunobjet;
+		}
+
 		transform.Translate (mouvement * Time.deltaTime);
+
 	}
 }
