d88500f640b935fbb1f1f31309444dc886af7860
Loïc M <mura.loic0@gmail.com>
2016-01-30 13:58:30 +0100

    Add boum when fight

---
+++ Assets/Scripts/LifeTime.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeTime : MonoBehaviour {
+	public float lifeTime = .5f;
+
+	void Awake() {
+		Destroy (gameObject, lifeTime);
+	}
+}
