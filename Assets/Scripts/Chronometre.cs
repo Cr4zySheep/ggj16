a19236fd09364b3ca0192d3ef48f6361cc724645
Vincent <vincent.bettenfeld@gmail.com>
2016-01-30 02:43:05 +0100

    Chrono et game Over

--- Assets/Scripts/Chronometre.cs
+++ Assets/Scripts/Chronometre.cs
@@ -1,28 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Chronometre : MonoBehaviour {
 
 	public Text label;
-	int leTemps = 120;
+	public int leTemps;
 	int minutes ;
 	int secondes;
+	int startingTime;
+
+	public List<GameObject> objetsActivables = new List<GameObject>();
+	public List<GameObject> objetsDesactivables = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-
+		startingTime = leTemps;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		leTemps = 120 - (int)Time.realtimeSinceStartup;
+		leTemps = startingTime - (int)Time.realtimeSinceStartup;
 		minutes = leTemps / 60;
 		secondes = leTemps % 60;
 		if (secondes > 9) {
 			label.text = minutes + ":" + secondes;
 		} else {
 			label.text = minutes + ":0" + secondes;
+		}
+		if ((int)leTemps<1) {
+			//GameObject.Find("GameOver").SetActive (true);
+			//GameObject.Find ("Player").Mouvement.SetActive(false);
+			foreach (GameObject i in objetsActivables) {
+				i.SetActive(true);
+			}
+			foreach (GameObject i in objetsDesactivables){
+				i.SetActive(false);
+			}
 		}
 	}
 }
