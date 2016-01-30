969ea3de79ceedfe915117b76845d43443475f07
Vincent <vincent.bettenfeld@gmail.com>
2016-01-30 11:15:34 +0100

    Menu principal

---
+++ Assets/Scripts/ChargerJeu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChargerJeu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChargerNiveau () {
+		SceneManager.LoadScene("wip");
+	}
+
+	public void Quitter(){
+		Application.Quit ();
+	}
+}
