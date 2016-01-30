e4434eef3af1e387b97acf56b30cf7a493031d32
Vincent <vincent.bettenfeld@gmail.com>
2016-01-30 14:41:06 +0100

    Possession toutes les 30s

--- Assets/Scripts/Chronometre.cs
+++ Assets/Scripts/Chronometre.cs
@@ -11,12 +11,26 @@ public class Chronometre : MonoBehaviour {
 	int minutes ;
 	int secondes;
 
+	public Text ScoreVert;
+	public Text ScoreJaune;
+	public Text ScoreHaut;
+	public Text ScoreBas;
+
 	public List<GameObject> objetsActivables = new List<GameObject>();
 	public List<GameObject> objetsDesactivables = new List<GameObject>();
+
+	public Possession player1;
+	public Possession player2;
+	List<Possession> joueurs = new List<Possession>();
+	public int numero;
+	float tempsPossession;
 
 	// Use this for initialization
 	void Start () {
 		tempsRestant = leTemps;
+		joueurs.Add (player1);
+		joueurs.Add (player2);
+		numero = Random.Range (1, 3);
 	}
 	
 	// Update is called once per frame
@@ -32,6 +46,16 @@ public class Chronometre : MonoBehaviour {
 			foreach (GameObject i in objetsActivables) {
 				i.SetActive (true);
 			}
+			// Affichage des scores réels et classement
+			if (int.Parse (ScoreJaune.text) > int.Parse (ScoreVert.text)) {
+				ScoreHaut.text = "Player 2 : " + int.Parse (ScoreJaune.text);
+				ScoreBas.text = "Player 1 : " + int.Parse (ScoreVert.text);
+			} else {
+				ScoreHaut.text = "Player 1 : " + int.Parse (ScoreVert.text);
+				ScoreBas.text = "Player 2 : " + int.Parse (ScoreJaune.text);
+			}
+			// AJOUTER LE CAS DE l'EGALITE
+
 			foreach (GameObject i in objetsDesactivables) {
 				i.SetActive (false);
 			}
@@ -39,5 +63,22 @@ public class Chronometre : MonoBehaviour {
 		} else if(tempsRestant > 0) {
 			tempsRestant -= Time.deltaTime;
 		}
+
+		//Gerer la possession
+
+		tempsPossession += Time.deltaTime;
+		if ((int) tempsPossession == 30) {
+			joueurs [numero % 2].possessed = true;
+			//Debug.Log (joueurs [numero % 2].possessed);
+		}
+		if ((int) tempsPossession == 45) {
+			joueurs [numero%2].possessed = false;
+			numero = numero + 1;
+			tempsPossession = 0;
+			//Debug.Log (joueurs [numero % 2].possessed);
+		}
+
 	}
+
+
 }
---
+++ Assets/Scripts/Possession.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Possession : MonoBehaviour {
+
+	public bool possessed;
+	// a passer en prive
+
+	void setPossessed(bool b){
+		this.possessed = b;
+	}
+
+	bool getPossessed(){
+		return this.possessed;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
