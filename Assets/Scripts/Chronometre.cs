411673193519715722b19165a0cb32dff9a0fe60
Vincent <vincent.bettenfeld@gmail.com>
2016-01-30 01:45:49 +0100

    Timer fonctionnel 2 minutes

---
+++ Assets/Scripts/Chronometre.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Chronometre : MonoBehaviour {
+
+	public Text label;
+	int leTemps = 120;
+	int minutes ;
+	int secondes;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		leTemps = 120 - (int)Time.realtimeSinceStartup;
+		minutes = leTemps / 60;
+		secondes = leTemps % 60;
+		if (secondes > 9) {
+			label.text = minutes + ":" + secondes;
+		} else {
+			label.text = minutes + ":0" + secondes;
+		}
+	}
+}
