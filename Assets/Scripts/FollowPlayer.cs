1d69aff13101405c5cd38709fbc8e9e9e67a3723
Loïc M <mura.loic0@gmail.com>
2016-01-30 08:49:10 +0100

    Add camera center on the both player

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
-	public GameObject player;
-	Vector3 offset;
+	public GameObject player1;
+	public GameObject player2;
 
-	// Use this for initialization
-	void Awake () {
-		offset = transform.position - player.transform.position;
-	}
-	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.position = player.transform.position + offset;
+		float x = (player1.transform.position.x + player2.transform.position.x) / 2;
+		float y = (player1.transform.position.y + player2.transform.position.y) / 2;
+		transform.position = new Vector3(x, y, transform.position.z);
+
+		float size = Mathf.Abs (player2.transform.position.x - player1.transform.position.x + player2.transform.position.y - player1.transform.position.y);
+		float potentialCameraSize = size;
+		if (potentialCameraSize < 2) {
+			potentialCameraSize = 2;
+		} else if (potentialCameraSize > 10) {
+			potentialCameraSize = 10;
+		}
+		Camera.main.orthographicSize = potentialCameraSize;
 	}
 }
